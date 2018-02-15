81bd31e1a5eb50a87596967f1a37afd729077a9d
Giuseppe <peppone.gc@gmail.com>
2018-02-15 12:03:50 +0100

    Create Setting Shared preferences

---
+++ app/src/main/java/com/example/arjun/vhf_school/xmpp/SettingsPrefs.java
@@ -0,0 +1,61 @@
+package com.example.arjun.vhf_school.xmpp;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by GCI on 15.02.2018.
+ */
+
+public class SettingsPrefs {
+    public static final String PREFS_NAME = "MyPrefsXMPP";
+
+    private Context context;
+    private String server;
+    private String user;
+    private String password;
+
+    public SettingsPrefs(Context context) {
+        this.context = context;
+        getMySharedPreferences();
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    private void getMySharedPreferences(){
+        SharedPreferences settings =  context.getSharedPreferences(PREFS_NAME, 0);
+        String server = settings.getString("server","85.1.228.64");
+        String user = settings.getString("user","user2");
+        String password = settings.getString("password","1234");
+    }
+}
