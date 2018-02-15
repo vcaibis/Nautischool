6afdf5a9d46065403cb8a779bbfcb4b2ac234f4a
vcaibis <vcaibis@gmail.com>
2018-02-15 13:28:29 +0100

    Add buttons method

--- app/src/main/java/com/example/arjun/vhf_school/mainActivity.java
+++ app/src/main/java/com/example/arjun/vhf_school/mainActivity.java
@@ -2,6 +2,8 @@ package com.example.arjun.vhf_school;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class mainActivity extends AppCompatActivity {
 
@@ -9,5 +11,52 @@ public class mainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ClickNumPad1(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "this is button 1", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void ClickNumPad2(View view) {
+    }
+
+    public void ClickNumPad3(View view) {
+    }
+
+    public void ClickNumPad4(View view) {
+    }
+
+    public void ClickNumPad5(View view) {
+    }
+
+    public void ClickNumPad6(View view) {
+    }
+
+    public void ClickNumPad7(View view) {
+    }
+
+    public void ClickNumPad8(View view) {
+    }
+
+    public void ClickNumPad9(View view) {
+    }
+
+    public void ClickNumPadCancel(View view) {
+    }
+
+    public void ClickNumPad0(View view) {
+    }
+
+    public void ClickNumPadEnter(View view) {
+    }
+
+    public void ClickDistressButton(View view) {
+    }
+
+    public void ClickDualWatchButton(View view) {
+    }
+
+    public void ClickButtonChannel_16(View view) {
     }
 }
