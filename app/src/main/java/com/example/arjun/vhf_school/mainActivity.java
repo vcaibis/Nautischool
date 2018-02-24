8f46488cb0dbc40166cd90ea8f9078da09109d0a
Giuseppe <peppone.gc@gmail.com>
2018-02-24 12:34:50 +0100

    Add constraintlayout to view
    Signed-off-by: zorgade <dzouda@gmail.com>

--- app/src/main/java/com/example/arjun/vhf_school/mainActivity.java
+++ app/src/main/java/com/example/arjun/vhf_school/mainActivity.java
@@ -17,6 +17,13 @@ public class mainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
     }
     //test
