5596ef1b02303097bf56ea90381b82820f6491cb
Helder <Osheed_@hotmail.com>
2018-03-02 17:33:16 +0100

    Creation package and Classes architecture

---
+++ app/src/main/java/com/example/arjun/vhf_school/ContextAndData/MachineUtils.java
@@ -0,0 +1,12 @@
+package com.example.arjun.vhf_school.ContextAndData;
+
+/**
+ * Created by Helder on 02.03.2018.
+ */
+//TODO: Helder
+
+/**
+ * STATIC GLOBAL variables, méthodes
+ */
+public class MachineUtils {
+}
---
+++ app/src/main/java/com/example/arjun/vhf_school/ContextAndData/ScreenLabels.java
@@ -0,0 +1,12 @@
+package com.example.arjun.vhf_school.ContextAndData;
+
+/**
+ * Created by Helder on 02.03.2018.
+ */
+//TODO: Helder
+
+/**
+ * contient les Valeurs des LABEL de l'écran
+ */
+public class ScreenLabels {
+}
