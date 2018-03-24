8c91950b708d596bb1314f46502a0aa79e36794c
zorgade <dzouda@gmail.com>
2018-03-24 11:36:13 +0100

    layout screen finished
    Signed-off-by: zorgade <dzouda@gmail.com>

--- app/src/main/java/ch/hevs/nautischool/mainActivity.java
+++ app/src/main/java/ch/hevs/nautischool/mainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPException;
@@ -14,6 +15,7 @@ import org.jivesoftware.smackx.muc.MultiUserChat;
 import org.jivesoftware.smackx.muc.MultiUserChatManager;
 
 import ch.hevs.nautischool.machine.MachineContext;
+import ch.hevs.nautischool.machine.ScreenLabels;
 
 public class mainActivity extends AppCompatActivity {
 //    static Context context;
@@ -57,21 +59,60 @@ public class mainActivity extends AppCompatActivity {
 
     }
 
+    public void radioToScreen(View view) {
+        ScreenLabels sc = radio.getScreenLabels();
+        TextView right1 = (TextView)findViewById(R.id.right1);
+        TextView right2 = (TextView)findViewById(R.id.right2);
+        TextView right3 = (TextView)findViewById(R.id.right3);
+        TextView right4 = (TextView)findViewById(R.id.right4);
+
+        right1.setText(sc.right1);
+        right2.setText(sc.right2);
+        right3.setText(sc.right3);
+        right4.setText(sc.right4);
+
+        TextView mid1 = (TextView)findViewById(R.id.mid1);
+        TextView mid2 = (TextView)findViewById(R.id.mid2);
+        TextView mid3 = (TextView)findViewById(R.id.mid3);
+        TextView mid4 = (TextView)findViewById(R.id.mid4);
+
+        mid1.setText(sc.mid1);
+        mid2.setText(sc.mid2);
+        mid3.setText(sc.mid3);
+        mid4.setText(sc.mid4);
+
+        TextView left1 = (TextView)findViewById(R.id.left1);
+        TextView left2 = (TextView)findViewById(R.id.left2);
+        TextView left3 = (TextView)findViewById(R.id.left3);
+        TextView left4 = (TextView)findViewById(R.id.left4);
+
+        left1.setText(sc.left1);
+        left2.setText(sc.left2);
+        left3.setText(sc.left3);
+        left4.setText(sc.left4);
+
+        TextView message1 = (TextView)findViewById(R.id.message1);
+        TextView message2 = (TextView)findViewById(R.id.message2);
+        TextView message3 = (TextView)findViewById(R.id.message3);
+
+        message1.setText(sc.message1);
+        message2.setText(sc.message2);
+        message3.setText(sc.message3);
+
+
+
+        /*radio.getCurrentState();
+        radio.getMachineData();
+        radio.getTimer();*/
+    }
+    public void sk1(View view) {
+        radio.softkey(1, false);
+        radioToScreen(view);
+    }
     //test
     public void ClickNumPad1(View view) {
-        String x = radio.getScreenLabels().left1;
-/*
-        MultiUserChatManager manager = MultiUserChatManager.getInstanceFor(MyXMPP.connection);
-        // Create a MultiUserChat using an XMPPConnection for a room
-        MultiUserChat muc2 = manager.getMultiUserChat(MyXMPP.WATERPLACE);
-        try {
-            muc2.sendMessage("Button 1 clicked by "+MyXMPP.connection.getUser());
-        } catch (XMPPException e) {
-            e.printStackTrace();
-        } catch (SmackException.NotConnectedException e) {
-            e.printStackTrace();
-        }
-*/
+        radio.volume(10);
+        radioToScreen(view);
 
     }
 
@@ -83,7 +124,6 @@ public class mainActivity extends AppCompatActivity {
         smallC.setVisibility(View.VISIBLE);
 
     }
-
 
     public void ClickNumPad3(View view) {
     }
@@ -127,13 +167,6 @@ public class mainActivity extends AppCompatActivity {
     }
 
     public void ClickChangeLight(View view) {
-        RelativeLayout r = findViewById(R.id.displayScreen);
-        int colorId = r.getSolidColor();
-
-        if (colorId == R.color.colorScreenOff) {
-            r.setBackgroundColor(R.drawable.led_layout_on);
-        } else
-            r.setBackgroundColor(R.drawable.led_layout_off);
     }
 
     public void btnMenu_onClick(View view) {
